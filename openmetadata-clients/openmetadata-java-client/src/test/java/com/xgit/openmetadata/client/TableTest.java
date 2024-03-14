fefa1a15f1522f93f3b4db233983b3a30405ebfc
superzhc <zhengchao0555@163.com>
2024-03-14 16:10:27 +0800

    work update

--- openmetadata-clients/openmetadata-java-client/src/test/java/com/xgit/openmetadata/client/TableTest.java
+++ openmetadata-clients/openmetadata-java-client/src/test/java/com/xgit/openmetadata/client/TableTest.java
@@ -1,146 +1,217 @@
 package com.xgit.openmetadata.client;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmetadata.client.api.TablesApi;
 import org.openmetadata.client.model.*;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import org.openmetadata.schema.services.connections.metadata.OpenMetadataConnection;
 
 public class TableTest extends OpenMetadataBaseTest {
 
-    TablesApi api;
+  TablesApi api;
 
-    @Before
-    public void setUp() throws Exception {
-        api = getClient().buildClient(TablesApi.class);
-    }
+  @Before
+  public void setUp() throws Exception {
+    api = getClient().buildClient(TablesApi.class);
+  }
 
-    @Test
-    public void testGetTables() {
-        Table table = api.getTableByFQN("superz_template_doris.default.demo.device_data", "*", "all");
-        System.out.println(table);
-    }
+//  @Override
+//  protected OpenMetadataConnection setHostPort(OpenMetadataConnection openMetadataConnection) {
+//    openMetadataConnection.setHostPort("http://10.90.20.236:8585/api");
+//    return openMetadataConnection;
+//  }
 
-    @Test
-    public void testCreateOrUpdateTable() {
-        CreateTable createTable = new CreateTable();
-        createTable.setName(String.format("sdk-table-%s", minute()));
-        createTable.setDescription("Create By SDK, " + minute());
-        createTable.setTableType(CreateTable.TableTypeEnum.REGULAR);
+  @Test
+  public void testList() {
+    TableList tableList =
+        api.listTables("tags", null, null, "数仓分层.DWS汇总层", null, 5, null, null, null);
+    System.out.println(tableList);
+  }
 
-        String monthWithYear = month();
-        createTable.setDatabaseSchema(String.format("superz_test_clickhouse.sdk-%s.sdk-schema-%s", monthWithYear, monthWithYear));
+  @Test
+  public void testGetTables() {
+    Table table = api.getTableByFQN("superz_template_doris.default.demo.device_data", "*", "all");
+    System.out.println(table);
+  }
 
-        List<Column> columns = new ArrayList<>();
-//        for (int i = 1; i <= 50; i++) {
-//            Column column = new Column();
-//            column.setName("sdk-c" + i);
-//            column.setDataType(Column.DataTypeEnum.STRING);
-//            column.setDataTypeDisplay("string");
-//            // column.setConstraint(Column.ConstraintEnum.NOT_NULL);
-//            columns.add(column);
-//        }
+  /** 创建或更新表 */
+  @Test
+  public void testCreateOrUpdateTable() {
+    CreateTable createTable = new CreateTable();
+    createTable.setName(String.format("sdk_%s", minute()));
+    createTable.setDescription("通过SDK创建的测试数据");
+    createTable.setTableType(CreateTable.TableTypeEnum.REGULAR);
+    createTable.setDatabaseSchema("superz_template_doris.default.superz");
 
-        //region 创建列
-        Column column;
-        // Id
-        column=new Column();
-        column.setName("id");
-        column.setDataType(Column.DataTypeEnum.INT);
-        columns.add(column);
+    // region 创建列
+    // Id
+    Column cId = new Column();
+    cId.setName("id");
+    cId.setDataType(Column.DataTypeEnum.INT);
+    cId.setDescription("唯一标识列");
+    createTable.addColumnsItem(cId);
 
-        // NUMBER
-        column = new Column();
-        column.setName("c_number");
-        column.setDataType(Column.DataTypeEnum.NUMBER);
-        columns.add(column);
+    // rId
+    Column cRId = new Column();
+    cRId.setName("r_id");
+    cRId.setDataType(Column.DataTypeEnum.INT);
+    cRId.setDescription("关联表唯一标识列");
+    createTable.addColumnsItem(cRId);
 
-        // INT
-        column = new Column();
-        column.setName("c_int");
-        column.setDataType(Column.DataTypeEnum.INT);
-        columns.add(column);
+    List<Column> columns = new ArrayList<>();
 
-        // FLOAT
-        column = new Column();
-        column.setName("c_float");
-        column.setDataType(Column.DataTypeEnum.FLOAT);
-        columns.add(column);
+    Column column;
+    // NUMBER
+    column = new Column();
+    column.setName("c_number");
+    column.setDataType(Column.DataTypeEnum.NUMBER);
+    column.setDisplayName("数值列");
+    columns.add(column);
 
-        // DECIMAL
-        column = new Column();
-        column.setName("c_decimal");
-        column.setDataType(Column.DataTypeEnum.DECIMAL);
-        columns.add(column);
+    // INT
+    column = new Column();
+    column.setName("c_int");
+    column.setDataType(Column.DataTypeEnum.INT);
+    column.setDisplayName("整数列");
+    columns.add(column);
 
-        // TIMESTAMP
-        column = new Column();
-        column.setName("c_timestamp");
-        column.setDataType(Column.DataTypeEnum.TIMESTAMP);
-        columns.add(column);
+    // FLOAT
+    column = new Column();
+    column.setName("c_float");
+    column.setDataType(Column.DataTypeEnum.FLOAT);
+    column.setDisplayName("浮点数列");
+    columns.add(column);
 
-        // DATETIME
-        column = new Column();
-        column.setName("c_datetime");
-        column.setDataType(Column.DataTypeEnum.DATETIME);
-        columns.add(column);
+    // DECIMAL
+    column = new Column();
+    column.setName("c_decimal");
+    column.setDataType(Column.DataTypeEnum.DECIMAL);
+    columns.add(column);
 
-        // STRING
-        column = new Column();
-        column.setName("c_string");
-        column.setDataType(Column.DataTypeEnum.STRING);
-        columns.add(column);
+    // TIMESTAMP
+    column = new Column();
+    column.setName("c_timestamp");
+    column.setDataType(Column.DataTypeEnum.TIMESTAMP);
+    column.setDisplayName("时间戳列");
+    columns.add(column);
 
-        // VARCHAR，只有对于char，varchar等部分类型要求 datalength不允许为空
-        column = new Column();
-        column.setName("c_varchar");
-        column.setDataType(Column.DataTypeEnum.VARCHAR);
-        column.setDataLength(255);
-        columns.add(column);
+    // DATETIME
+    column = new Column();
+    column.setName("c_datetime");
+    column.setDataType(Column.DataTypeEnum.DATETIME);
+    column.setDisplayName("时间列");
+    columns.add(column);
 
-        /*
-        column=new Column();
-        column.setName("c_");
-        column.setDataType(Column.DataTypeEnum);
-        columns.add(column);
-        * */
+    // STRING
+    column = new Column();
+    column.setName("c_string");
+    column.setDataType(Column.DataTypeEnum.STRING);
+    column.setDisplayName("字符串列");
+    columns.add(column);
 
-        column=new Column();
-        column.setName("c_unique");
-        column.setDataType(Column.DataTypeEnum.STRING);
-        columns.add(column);
+    // VARCHAR，只有对于char，varchar等部分类型要求 datalength不允许为空
+    column = new Column();
+    column.setName("c_varchar");
+    column.setDataType(Column.DataTypeEnum.VARCHAR);
+    column.setDataLength(255);
+    columns.add(column);
 
+    /*
+    column=new Column();
+    column.setName("c_");
+    column.setDataType(Column.DataTypeEnum);
+    columns.add(column);
+    * */
+    createTable.getColumns().addAll(columns);
 
-        createTable.setColumns(columns);
-        // endregion
+    /*排序列*/
+    Column cSort = new Column();
+    cSort.setName("c_sort");
+    cSort.setDataType(Column.DataTypeEnum.STRING);
+    cSort.setDisplayName("排序列");
+    createTable.addColumnsItem(cSort);
 
-//        // 扩展，未定义自定义属性，会报错
-//        Map<String,Object> extension=new HashMap<>();
-//        extension.put("partionBy","id");
-//        createTable.extension(extension);
+    /*唯一值列*/
+    Column cUnique = new Column();
+    cUnique.setName("c_unique");
+    cUnique.setDataType(Column.DataTypeEnum.STRING);
+    cUnique.setDisplayName("唯一值列");
+    createTable.addColumnsItem(cUnique);
 
-        // 添加约束：主键
-        TableConstraint primaryKey=new TableConstraint();
-        primaryKey.addColumnsItem("c_string").addColumnsItem("c_varchar");
-        primaryKey.constraintType(TableConstraint.ConstraintTypeEnum.PRIMARY_KEY);
-        createTable.addTableConstraintsItem(primaryKey);
+    /*分区列*/
+    Column p1 = new Column();
+    p1.setName("p1");
+    p1.setDataType(Column.DataTypeEnum.STRING);
+    p1.setDescription("分区列1");
+    createTable.addColumnsItem(p1);
 
-        TableConstraint unique;
-        // 添加约束：唯一性
-        unique=new TableConstraint();
-        unique.addColumnsItem("c_unique");
-        unique.constraintType(TableConstraint.ConstraintTypeEnum.UNIQUE);
-        createTable.addTableConstraintsItem(unique);
+    Column p2 = new Column();
+    p2.setName("p2");
+    p2.setDataType(Column.DataTypeEnum.STRING);
+    p2.setDescription("分区列2");
+    createTable.addColumnsItem(p2);
 
-        // 分区
-        TablePartition tablePartition=new TablePartition();
-        tablePartition.addColumnsItem("c_timestamp");
-        createTable.tablePartition(tablePartition);
+    Column p3 = new Column();
+    p3.setName("p3");
+    p3.setDataType(Column.DataTypeEnum.STRING);
+    p3.setDescription("分区列3");
+    createTable.addColumnsItem(p3);
+    // endregion
 
-        Table table = api.createOrUpdateTable(createTable);
-        System.out.println(table);
-    }
+    // 扩展，未定义自定义属性，会报错
+    Map<String, Object> extension = new HashMap<>();
+    extension.put(
+        "createTime",
+        // 值类型任意
+        // new Date()
+        // LocalDateTime.now()
+        // "abcd"
+        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")));
+    createTable.extension(extension);
+
+    // 添加约束：主键
+    TableConstraint primaryKey = new TableConstraint();
+    primaryKey.addColumnsItem(cId.getName());
+    primaryKey.constraintType(TableConstraint.ConstraintTypeEnum.PRIMARY_KEY);
+    createTable.addTableConstraintsItem(primaryKey);
+
+    // 添加约束：唯一性
+    TableConstraint unique;
+    unique = new TableConstraint();
+    unique.addColumnsItem(cUnique.getName());
+    unique.constraintType(TableConstraint.ConstraintTypeEnum.UNIQUE);
+    createTable.addTableConstraintsItem(unique);
+
+    // 添加约束：外键
+    TableConstraint foreignKey = new TableConstraint();
+    foreignKey.addColumnsItem(cRId.getName());
+    foreignKey.constraintType(TableConstraint.ConstraintTypeEnum.FOREIGN_KEY);
+    createTable.addTableConstraintsItem(foreignKey);
+
+    // 添加约束：排序键
+    TableConstraint sortKey = new TableConstraint();
+    sortKey.addColumnsItem(cSort.getName());
+    sortKey.constraintType(TableConstraint.ConstraintTypeEnum.SORT_KEY);
+    createTable.addTableConstraintsItem(sortKey);
+
+    // 分区
+    TablePartition tablePartition = new TablePartition();
+    tablePartition.columns(Arrays.asList(p1.getName(), p2.getName(), p3.getName()));
+    createTable.tablePartition(tablePartition);
+
+    // 标签
+    TagLabel dwLevel = new TagLabel();
+    dwLevel.name("ODS贴源层");
+    dwLevel.source(TagLabel.SourceEnum.CLASSIFICATION);
+    dwLevel.tagFQN("数仓分层.ODS贴源层");
+    dwLevel.labelType(TagLabel.LabelTypeEnum.MANUAL);
+    dwLevel.state(TagLabel.StateEnum.CONFIRMED);
+    createTable.addTagsItem(dwLevel);
+
+    Table table = api.createOrUpdateTable(createTable);
+    System.out.println(table);
+  }
 }
